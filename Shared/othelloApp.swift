a7c92194238569abb9f7267a3fea075a00782329
wzhao18 <740286700@qq.com>
2022-01-11 15:03:03 -0500

    first commit

---
+++ Shared/othelloApp.swift
@@ -0,0 +1,17 @@
+//
+//  othelloApp.swift
+//  Shared
+//
+//  Created by 提莫 on 2022-01-11.
+//
+
+import SwiftUI
+
+@main
+struct othelloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
